92230b9c649ef86188f31c6f6d85f2d6a932c6e8
Niroshan Ranapathi <niroshanrd.13@cse.mrt.ac.lk>
2018-02-16 16:25:44 +0530

    update user password

--- src/test/java/com/sysco/ordermanager/travis.sql
+++ src/test/java/com/sysco/ordermanager/travis.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE sysco;
 #Create Testuser
-CREATE USER 'omadmin'@'localhost' IDENTIFIED BY '';
+CREATE USER 'omadmin'@'localhost' IDENTIFIED BY '12345';
 GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON *.* TO 'dev'@'localhost';
